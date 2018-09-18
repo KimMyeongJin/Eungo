eb65c22e405b5c85548fab6aecf621b8e5194c6c
idsiemu <idsiemu@gmail.com>
2018-09-18 13:48:35 +0900

    BaordDAO -  smart_select 수정 & SubmitLodaAction 추가

---
+++ mvc_Eungo/Eungo/src/com/eungo/controller/board/SubmitLoadAction.java
@@ -0,0 +1,40 @@
+package com.eungo.controller.board;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.eungo.action.Action;
+import com.eungo.dao.MemberDAO;
+import com.eungo.dto.MemberVO;
+import com.eungo.util.DBManager;
+import com.eungo.util.Script;
+
+public class SubmitLoadAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String url = "account/submit-service.jsp";
+		String index_url = "index.jsp";
+		
+		String phonenumber = null;
+		MemberVO member = new MemberVO();
+		MemberDAO dao = new MemberDAO();
+		
+		HttpSession session = request.getSession();
+		String email = session.getAttribute("email").toString();
+		if (session.getAttribute("email") != null) {
+			phonenumber = dao.select_phonenumber(email);
+			request.setAttribute("phonenumber", phonenumber);
+			RequestDispatcher dis = request.getRequestDispatcher(url);
+			dis.forward(request, response);
+		}else {
+			Script.moving(response, "세션이 만료되었습니다.", index_url);
+		}
+	}
+}
